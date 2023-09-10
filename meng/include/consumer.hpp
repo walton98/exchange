d1d1afa8d6330dcceebb213cd3bb7a1d888fdc47
Sam Walton <samw98@live.co.uk>
2023-09-10 21:40:06 +0100

    server: static polymorphism

--- meng/include/consumer.hpp
+++ meng/include/consumer.hpp
@@ -4,8 +4,9 @@
 
 namespace meng {
 
-class consumer : public server::consumer {
-  using server::consumer::consumer;
+class consumer : public server::consumer<consumer> {
+  friend server::consumer<consumer>;
+  using server::consumer<consumer>::consumer;
 
   void handle_message(char data[]) {
     std::cout << "handled message " << data << std::endl;
