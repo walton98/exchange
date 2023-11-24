8e3a8b2e2de1e0fe657e38b55210685d55085ffd
Sam Walton <samw98@live.co.uk>
2023-11-24 16:26:07 +0000

    matcher: create snapshots

--- matcher/src/engine.cpp
+++ matcher/src/engine.cpp
@@ -2,6 +2,13 @@
 
 namespace matcher {
 
+engine::~engine() {
+  // wait for any writer threads to complete
+  if (snapshot_future_.valid()) {
+    snapshot_future_.wait();
+  }
+}
+
 std::expected<void, engine_error>
 engine::operator()(const request::create_book &request) {
   books_.create_book(request.id());
@@ -19,7 +26,22 @@ engine::operator()(const request::create_order &request) {
 
 std::expected<void, engine_error>
 engine::operator()(const request::snapshot &request) {
+  using namespace std::chrono_literals;
+
+  if (snapshot_future_.valid() &&
+      snapshot_future_.wait_for(0ms) != std::future_status::ready) {
+    std::cout << "skipping snapshot" << std::endl;
+    return std::unexpected{engine_error::snapshot_in_progress};
+  }
+
   std::cout << "snapshotting" << std::endl;
+  // copy registry in memory to another thread,
+  // and write to disk in thread.
+  auto writer_fn = [fn = snapshot_file_, reg = books_]() {
+    save_engine_registry(reg, fn);
+  };
+  snapshot_future_ = std::async(std::launch::async, writer_fn);
+
   return {};
 }
 
