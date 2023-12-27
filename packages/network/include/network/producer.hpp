551b03278cf8a0c57c28206685552d20d05372d3
Sam Walton <samw98@live.co.uk>
2023-12-27 16:18:40 +0000

    network: replace use_awaitable with deferred
    - use `asio::deferred` in place of `asio::use_awaitable`
      - prevents the allocation of a coroutine frame
    - use `asio::as_tuple` in place of `asio::redirect_error`

--- packages/network/include/network/producer.hpp
+++ packages/network/include/network/producer.hpp
@@ -1,10 +1,11 @@
 #include <print>
-#include <vector>
+#include <span>
 
+#include <asio/as_tuple.hpp>
+#include <asio/awaitable.hpp>
+#include <asio/deferred.hpp>
 #include <asio/io_context.hpp>
 #include <asio/ip/udp.hpp>
-#include <asio/redirect_error.hpp>
-#include <asio/use_awaitable.hpp>
 
 namespace network {
 
@@ -15,11 +16,9 @@ public:
         endpoint_{*asio::ip::udp::resolver(ioc).resolve(asio::ip::udp::v4(),
                                                         host, port)} {}
 
-  asio::awaitable<void> produce(std::vector<char> data) {
-    asio::error_code ec;
-    co_await socket_.async_send_to(
-        asio::buffer(data), endpoint_,
-        asio::redirect_error(asio::use_awaitable, ec));
+  asio::awaitable<void> produce(std::span<char> data) {
+    auto [ec, _] = co_await socket_.async_send_to(
+        asio::buffer(data), endpoint_, asio::as_tuple(asio::deferred));
     if (ec) {
       std::println("Error processing message: {}", ec.message());
     }
